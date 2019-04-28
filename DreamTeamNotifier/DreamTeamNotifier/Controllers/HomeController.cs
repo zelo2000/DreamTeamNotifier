8f8e178aa8154d7b9b1606186b3b4129ebe9f435
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2019-04-28 18:09:38 +0300

    Add project and Telegram.Bot API

---
+++ DreamTeamNotifier/DreamTeamNotifier/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DreamTeamNotifier.Models;
+
+namespace DreamTeamNotifier.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello world!";
+        }
+    }
+}
