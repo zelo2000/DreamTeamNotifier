48ea7dbb0b03b2e487ae11be8e8f8d1e574aab79
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2019-05-01 15:16:45 +0300

    add wather command

---
+++ DreamTeamNotifier/DTN.Core/Commands/Weather/GetLocationCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace DTN.Core.Commands.Weather
+{
+    public class GetLocationCommand : Command
+    {
+        public override string Name => @"/set_location";
+
+        public override bool Contains(Message message)
+        {
+            return message.Type == MessageType.Location;
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient client)
+        {
+            var location = message.Location;
+            //TODO get weather
+            var chatId = message.Chat.Id;
+            var replyMessage = "";
+            await client.SendTextMessageAsync(chatId, replyMessage);
+        }
+    }
+}
---
+++ DreamTeamNotifier/DTN.Core/Commands/Weather/GetWeatherCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DTN.Core.Commands.Weather
+{
+    public class GetWeatherCommand : Command
+    {
+        public override string Name => @"/get_weather";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            await client.SendTextMessageAsync(chatId, "Please input location using special message");
+        }
+
+    }
+}
